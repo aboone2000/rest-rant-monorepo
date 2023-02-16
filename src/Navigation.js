71b852655ec58f3ecd34df3b431537d9218f11aa
Aaron Boone <37162732+aboone2000@users.noreply.github.com>
2023-02-16 13:24:20 -0500

    Add files via upload

---
+++ src/Navigation.js
@@ -0,0 +1,59 @@
+import { useState, useEffect, useContext } from 'react'
+import { useHistory } from "react-router";
+import { CurrentUser } from './contexts/CurrentUser';
+
+function Navigation() {
+
+    const history = useHistory()
+
+    const { currentUser } = useContext(CurrentUser)
+
+    let loginActions = (
+        <>
+            <li style={{ float: 'right' }}>
+                <a href="#" onClick={() => history.push("/sign-up")}>
+                    Sign Up
+                </a>
+            </li>
+            <li style={{ float: 'right' }}>
+                <a href="#" onClick={() => history.push("/login")}>
+                    Login
+                </a>
+            </li>
+        </>
+    )
+
+      
+
+if (currentUser) {
+    loginActions = (
+        <li style={{ float: 'right' }}>
+            Logged in as {currentUser.firstName} {currentUser.lastName}
+        </li>
+    )
+}
+
+let addPlaceButton = null
+
+if (currentUser?.role === 'admin') {
+    addPlaceButton = (
+        <li>
+            <a href="#" onClick={() => history.push("/places/new")}>
+                Add Place
+            </a>
+        </li>
+    )
+}
+
+return (
+    <nav>
+          
+            {addPlaceButton}
+            {loginActions}
+        
+    </nav>
+
+    )
+}
+
+export default Navigation;
